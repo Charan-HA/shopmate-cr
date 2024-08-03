f3a3edab83533a1eba95ef8a6532ffa097fbf819
Charan-HA <charanha2001charu@gmail.com>
2024-08-03 19:39:23 +0530

    Completed Files

---
+++ src/components/ProductCard.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useCart } from "../context/CartContext";
+import "./ProductCard.css";
+
+export const ProductCard = ({product}) => {
+  const [isInCart, setIsInCart] = useState(false);
+  const { cartList, addToCart, removeFromCart} =useCart();
+  const {id, name, price, image} = product;
+
+  useEffect(() =>{
+    const productIsINCart = cartList.find(cartItem => cartItem.id === id );
+    if(productIsINCart){
+      setIsInCart(true);
+    }else{
+      setIsInCart(false);
+    } 
+  }, [cartList, id]);
+
+  return (
+    <div className="productCard">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <div className="action">
+        <p>${price}</p>
+        { isInCart ? (<button className="remove" onClick={() =>removeFromCart(product) }>Remove</button>) : (<button onClick={() =>addToCart(product) }>Add To Cart</button>)}
+      </div>
+    </div>
+  )
+}
+
